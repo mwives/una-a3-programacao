4c55b95b5510e00f1351e4abe594eb08cdb15b24
Ives M. Watanabe <ivesmw@gmail.com>
2022-11-15 22:37:26 -0300

    ajustes menu

--- src/Main.java
+++ src/Main.java
@@ -3,53 +3,37 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(" 1. Cadastro de mesa\n 2. Remoção de mesa\n 3. Busca mesa pelo número\n 4. Busca mesa pela capacidade de clientes\n 5. relatório de mesas\n 0. sair\n");
-        int menu = sc.nextInt();
 
+        int opcao;
 
-        switch (menu){
+        do {
+            // TODO: Deixar o menu bonitinho :)
+            System.out.println(" 1. Cadastro de mesa\n 2. Remoção de mesa\n 3. Busca mesa pelo número\n 4. Busca mesa pela capacidade de clientes\n 5. relatório de mesas\n 0. sair\n");
 
-            case 1:
+            opcao = sc.nextInt();
 
-                System.out.println("informar o numero da mesa, a situação e a capacidade máxima");
-                System.out.println("Cadastrar mesa");
-                break ;
-
-            case 2:
-
-                System.out.println("Mesa removida");
-                break ;
-
-            case 3:
-
-                System.out.println("Mesa encontrada");
-                break;
-
-            case 4:
-
-                System.out.println("Mesa encontrada ");
-                break;
-
-            case 5:
-
-                System.out.println("Relatorio das mesas");
-                break;
-
-            case 0:
-                sc.close();
-                break;
-
-
-
-
-
-
-
-
-        }
-
-
-
-        }
-
+            switch (opcao) {
+                case 1:
+                    System.out.println("Cadastrar mesa");
+                    break;
+                case 2:
+                    System.out.println("Mesa removida");
+                    break;
+                case 3:
+                    System.out.println("Mesa encontrada");
+                    break;
+                case 4:
+                    System.out.println("Mesa encontrada ");
+                    break;
+                case 5:
+                    System.out.println("Relatorio das mesas");
+                    break;
+                case 0:
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+                    break;
+            }
+        } while (opcao != 0);
     }
+}
